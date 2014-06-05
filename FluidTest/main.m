582e20978ed7f2f5718e293752df3149efe94633
Mason Silber <mason@twitter.com>
2014-06-05 10:46:24 -0700

    Initial Commit

---
+++ FluidTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FluidTest
+//
+//  Created by Mason Silber on 6/5/14.
+//  Copyright (c) 2014 Mason Silber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
